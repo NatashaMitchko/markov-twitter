14a3730f605eb45ee1805e821f59601dd17da29c
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-12 17:31:12 -0700

    Implemented tweet of 140 characters and loops until user quits

--- markov.py
+++ markov.py
@@ -56,14 +56,30 @@ def make_text(chains):
         words.append(word)
         key = (key[1], word)
 
-    return " ".join(words)
+    new_text = " ".join(words)
+    return new_text[:140]
 
 
 def tweet(chains):
     # Use Python os.environ to get at environmental variables
     # Note: you must run `source secrets.sh` before running this file
     # to make sure these environmental variables are set.
-    pass
+    text = make_text(chains)
+
+    api = twitter.Api(
+        consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+        consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+        access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+        access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+    )
+
+    print api.VerifyCredentials()
+
+    status = api.PostUpdate(text)
+
+    print status.text
+
+tweet_again = raw_input("\nEnter to tweet again [q to quit] > ")
 
 # Get the filenames from the user through a command line prompt, ex:
 # python markov.py green-eggs.txt shakespeare.txt
@@ -77,3 +93,9 @@ chains = make_chains(text)
 
 # Your task is to write a new function tweet, that will take chains as input
 # tweet(chains)
+
+tweet(chains)
+
+while tweet_again != "q":
+    tweet(chains)
+    tweet_again = raw_input("Enter to tweet again [q to quit] > ")
